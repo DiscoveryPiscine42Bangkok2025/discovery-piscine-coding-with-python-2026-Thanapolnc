54b93214409165bb48fc90a810ec38a4d684ddd7
Thanapolnc <66070077@kmitl.ac.th>
2026-02-10 21:52:15 +0700

    Cellule4.3

---
+++ cell04/ex03/float.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+number = (input("Give me a number: "))
+
+ans = float(number)
+
+if ans % 1 == 0:
+  print("The number is an integer.")
+else:
+  print("This number is a decimal.")
+
+
+# print(type(num))
+
