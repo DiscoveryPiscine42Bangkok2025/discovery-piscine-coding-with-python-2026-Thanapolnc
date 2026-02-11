62e130ec076e6181964e052f4bece4840ac59192
Thanapolnc <66070077@kmitl.ac.th>
2026-02-11 14:55:11 +0700

    Cellule5.1

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+numbers = [2, 8, 9, 48, 8, 22, -12, 2]
+
+newnumbers = []
+
+for i in numbers:
+  newnumbers.append(i+2)
+
+print("Original array:", numbers)
+print("New array:", newnumbers)
