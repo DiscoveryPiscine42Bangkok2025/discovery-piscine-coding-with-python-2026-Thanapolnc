e46f4691366228a765d288eb472502d959e81e60
Thanapolnc <66070077@kmitl.ac.th>
2026-02-11 15:40:17 +0700

    Cellule5.2

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+numbers = [2, 8, 9, 48, 8, 22, -12, 2]
+
+newnumbers = []
+
+for i in numbers:
+  if i > 5:
+    newnumbers.append(i+2)
+
+print(numbers)
+print(newnumbers)
